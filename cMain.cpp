99442c48a81390ae65afd493a984a730209a947e
Colin Bakker <135772044+colinjbakker@users.noreply.github.com>
2024-08-05 15:55:56 -0500

    Fixed issues with dragging window
    Issue when dragging window too fast, removed listeners for mouse up and mouse move.

--- cMain.cpp
+++ cMain.cpp
@@ -1,9 +1,9 @@
 #include "cMain.h"
 
-#define GRAVITY 0.15
-#define INTERVAL 10
-#define FRICTION 0.1
-#define BOUNCE 0.5
+unsigned const int INTERVAL = 5;
+const float GRAVITY = 0.2;
+const float FRICTION = 0.25;
+const float BOUNCINESS = 0.5;
 
 wxBEGIN_EVENT_TABLE(cMain, wxFrame)
 	EVT_TIMER(10001, cMain::OnProgressTimer)
@@ -18,9 +18,8 @@ cMain::cMain() : wxFrame(nullptr, wxID_ANY, "", wxPoint(400,30), wxSize(200, 200
 
 	pos = wxRealPoint(400.0, 30.0);
 	m_panel->Bind(wxEVT_LEFT_DOWN, &cMain::OnMouseDown, this);
-	m_panel->Bind(wxEVT_LEFT_UP, &cMain::OnMouseUp, this);
-	m_panel->Bind(wxEVT_MOTION, &cMain::OnMouseMove, this);
 	time = wxGetLocalTimeMillis().ToDouble();
+	
 	vx = -4.0;
 	vy = 0.0;
 	ax = 0.0;
@@ -42,42 +41,8 @@ void cMain::OnProgressTimer(wxTimerEvent& event)
 {
 	double dt = (wxGetLocalTimeMillis() - time).ToDouble() / 10.0;
 	time = wxGetLocalTimeMillis().ToDouble();
-
-	pos.x = this->GetScreenPosition().x + (vx * dt);
-	pos.y = this->GetScreenPosition().y + (vy * dt);
-	vx = vx > 0 ? std::max(vx + (ax * dt), 0.0) : std::min(vx + (ax * dt), 0.0);
-	vy = vy + (ay * dt);
-	int h = this->GetSize().GetHeight();
-	int w = this->GetSize().GetWidth();
-
-	if (pos.y > wxGetDisplaySize().GetHeight() - h || pos.y < 0) {
-		ay = pos.y < 0 ? GRAVITY : 0;
-		if (vx == 0) {
-			ax = 0;
-		}
-		else {
-			ax = (vx > 0) ? -FRICTION : FRICTION;
-		}
-		vy = (vy > 0 && vy < 2) || (vy < 0 && vy > -2) ? 0 : - vy * BOUNCE;
-		pos.y = pos.y < 0 ? 0 : wxGetDisplaySize().GetHeight() - h;
-	}
-	else {
-		ax = 0;
-		ay = GRAVITY;
-	}
-
-	if (pos.x > wxGetDisplaySize().GetWidth() - w || pos.x < 0) {
-		vx = -vx * BOUNCE;
-		ax = -ax;
-		pos.x = (pos.x < 0) ? 0 : wxGetDisplaySize().GetWidth() - w;
-	}
-	this->Move(pos);
-	event.Skip();
-}
-
-void cMain::OnMouseMove(wxMouseEvent& event) {
-	if (event.Dragging()) {
-		wxRealPoint newPos = ClientToScreen(event.GetPosition() - dragStart);
+	if (drag && wxGetMouseState().LeftIsDown()) {
+		wxPoint newPos = wxGetMousePosition() - dragStart;
 		wxPoint diff = wxPoint(newPos.x - pos.x, newPos.y - pos.y);
 		pos = newPos;
 		this->Move(pos);
@@ -86,20 +51,44 @@ void cMain::OnMouseMove(wxMouseEvent& event) {
 		vx = diff.x;
 		vy = diff.y;
 	}
-}
+	else {
+		drag = false;
 
-void cMain::OnMouseDown(wxMouseEvent& event) {
-	if (m_timer->IsRunning()) {
-		m_timer->Stop();
+		const unsigned int floorY = wxGetDisplaySize().GetHeight() - this->GetSize().GetHeight();
+		const unsigned int wallX = wxGetDisplaySize().GetWidth() - this->GetSize().GetWidth();
+
+		pos.x = this->GetScreenPosition().x + (vx * dt);
+		pos.y = this->GetScreenPosition().y + (vy * dt);
+
+		vx = vx > 0 ? std::max(vx + (ax * dt), 0.0) : std::min(vx + (ax * dt), 0.0);
+		vy = vy + (ay * dt);
+		
+
+		ax = 0;
+		ay = GRAVITY;
+
+		if (pos.y >= floorY || pos.y < 0) {
+			//wxLogDebug("before: " + wxString::Format(wxT("%f"), vy));
+			ay = (pos.y < 0) ? GRAVITY : 0;
+			ax = (vx == 0) ? 0 : (vx > 0) ? -FRICTION : FRICTION;
+			vy = (vy > -2 && vy < 2) ? 0 : - (vy - GRAVITY * 2 * dt) * BOUNCINESS;
+			pos.y = (pos.y < 0) ? 0 : floorY;
+			//wxLogDebug("after: " + wxString::Format(wxT("%f"), vy));
+		}
+
+		if (pos.x >= wallX || pos.x <= 0) {
+			vx = -vx * BOUNCINESS;
+			ax = -ax;
+			pos.x = (pos.x < 0) ? 0 : wallX;
+		}
+		this->Move(pos);
 	}
-	dragStart = event.GetPosition();
+	
 	event.Skip();
 }
 
-void cMain::OnMouseUp(wxMouseEvent& event) {
-	if (!(m_timer->IsRunning())) {
-		time = wxGetLocalTimeMillis().ToDouble();
-		m_timer->Start();
-	}
+void cMain::OnMouseDown(wxMouseEvent& event) {
+	dragStart = event.GetPosition();
+	drag = true;
 	event.Skip();
 }
